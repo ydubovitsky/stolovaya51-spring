de15813637d853157f7511fc62d33cc12b486a08
user <y.dubovitsky@gmail.com>
2023-01-23 10:21:16 +0300

    Init project

---
+++ src/test/java/ru/ydubovitsky/diningroomniitp/DiningRoomNiitpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ydubovitsky.diningroomniitp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRoomNiitpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

39bca95403f13ff36a341e568eb3b12ab0e092e2
user <y.dubovitsky@gmail.com>
2023-01-30 15:15:46 +0300

    MenuController redesigned and expanded by new Put method

--- src/main/java/ru/ydubovitsky/diningroomniitp/api/MenuController.java
+++ src/main/java/ru/ydubovitsky/diningroomniitp/api/MenuController.java
@@ -6,23 +6,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.ydubovitsky.diningroomniitp.dto.MenuRequestDto;
 import ru.ydubovitsky.diningroomniitp.model.Menu;
-import ru.ydubovitsky.diningroomniitp.service.MenuService;
+import ru.ydubovitsky.diningroomniitp.service.MenuServiceInterface;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/menu")
 @RequiredArgsConstructor
 public class MenuController {
 
-    private final MenuService menuService;
+    private final MenuServiceInterface menuService;
 
     @PostMapping
     public HttpStatus addNewMenu(@RequestBody MenuRequestDto menuRequestDto) {
-        menuService.createNewMenu(menuRequestDto);
+        menuService.saveMenu(menuRequestDto);
         return HttpStatus.OK;
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateMenu(@RequestBody MenuRequestDto menuRequestDto) {
+        Menu menu = menuService.updateMenu(menuRequestDto);
+        return ResponseEntity.ok(menu);
     }
 
     @GetMapping(params = {"day", "month", "year"})
@@ -31,13 +36,13 @@ public class MenuController {
             @RequestParam(value = "month") String month,
             @RequestParam(value = "year") String year
     ) {
-        List<Menu> menuByDayMonthYear = menuService.getMenuByYearMonthDay(year, month, day);
+        Menu menuByDayMonthYear = menuService.getMenuByYearMonthDay(year, month, day);
         return ResponseEntity.ok(menuByDayMonthYear);
     }
 
     @GetMapping(params = {"date"})
     public ResponseEntity<?> getMenuByDate(@RequestParam(value = "date") LocalDate date) {
-        List<Menu> menuByDayMonthYear = menuService.getMenuByDate(Date.valueOf(date));
+        Menu menuByDayMonthYear = menuService.getMenuByDate(Date.valueOf(date));
         return ResponseEntity.ok(menuByDayMonthYear);
     }
 }

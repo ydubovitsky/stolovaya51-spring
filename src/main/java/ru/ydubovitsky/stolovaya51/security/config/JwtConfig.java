1c79e57456c060e2180897e1a4403b7b3270c551
y-dubovitsky <pXmJ5M9dvP>
2023-11-11 16:11:48 +0300

    Inside SecurityConfig added new implementation of CORSConfig that uses application variables

--- src/main/java/ru/ydubovitsky/stolovaya51/security/config/JwtConfig.java
+++ src/main/java/ru/ydubovitsky/stolovaya51/security/config/JwtConfig.java
@@ -1,10 +1,12 @@
 package ru.ydubovitsky.stolovaya51.security.config;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import ru.ydubovitsky.stolovaya51.utils.ApplicationVariablesUtils;
 
 @NoArgsConstructor
 @Getter @Setter
@@ -22,5 +24,13 @@ public class JwtConfig {
 
     @Value("${application.admin.password}")
     private String initAdminPassword;
+
+    @Getter(AccessLevel.NONE)
+    @Value("${application.cors.allowedOriginsArray}")
+    private String allowedOriginsArray;
+
+    public String[] getAllowedOriginsArray() {
+        return ApplicationVariablesUtils.splitStringToArrayByPattern(allowedOriginsArray);
+    }
 
 }
--- src/main/java/ru/ydubovitsky/stolovaya51/security/config/SecurityConfig.java
+++ src/main/java/ru/ydubovitsky/stolovaya51/security/config/SecurityConfig.java
@@ -1,18 +1,23 @@
 package ru.ydubovitsky.stolovaya51.security.config;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.ydubovitsky.stolovaya51.security.filter.JwtTokenVerifierFilter;
 import ru.ydubovitsky.stolovaya51.security.filter.JwtUsernameAndPasswordAuthFilter;
 
+@Slf4j
 @Configuration
 @AllArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private final JwtConfig jwtConfig;
 
@@ -24,6 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //! Проблема в том, что эти два фильтра не управляются спрингом, т.к. мы создали их через new()
         http.addFilter(new JwtUsernameAndPasswordAuthFilter(authenticationManager(), jwtConfig));
         http.addFilterAfter(new JwtTokenVerifierFilter(jwtConfig), JwtUsernameAndPasswordAuthFilter.class);
+    }
+
+    //https://sysout.ru/nastrojka-cors-v-spring-security/
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        log.info(String.format("CORS AllowedOrigins %s", jwtConfig.getAllowedOriginsArray()));
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(jwtConfig.getAllowedOriginsArray())
+                        .allowedMethods("*");
+            }
+        };
     }
 
 }
